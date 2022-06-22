b81aa0a7c43a0c8fa08e9c7aba6627f1da6e2f96
Craig Gardner <craig.gardner1@gmail.com>
2022-06-22 14:18:47 -0500

    Added WEBDEV104 and rearranged folder structure

---
+++ WEBDEV104/104_1/subscriber/subscriber.py
@@ -0,0 +1,14 @@
+import redis
+import time
+
+r = redis.Redis(host='redis-server', port=6379, db=0)
+
+p = r.pubsub()
+p.subscribe("food_info")
+
+while True:
+    message = p.get_message()
+    if message:
+        print(message)
+        print(message['data'])
+    time.sleep(0.01)
